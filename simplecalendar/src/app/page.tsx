462b359e2edb22ab819555ce5234053a4b520f97
BlackSquirrel <Maexxla3458@proton.me>
2025-01-08 18:37:41 +0100

    19:48

--- simplecalendar/src/app/page.tsx
+++ simplecalendar/src/app/page.tsx
@@ -5,8 +5,50 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin, { Draggable, DropArg } from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid' 
+import { Fragment, useEffect, useState } from 'react'
+
+interface Event {
+  title: string;
+  start: Date | string;
+  allDay: boolean;
+  id: number;
+}
 
 export default function Home() {
+  const [events, setEvents] = useState([
+    { title: 'event 1', id: '1' },
+    { title: 'event 2', id: '2' },
+    { title: 'event 3', id: '3' },
+    { title: 'event 4', id: '4' },
+    { title: 'event 5', id: '5' },
+  ])
+  const [allEvents, setAllEvents] = useState<Event[]>([])
+  const [showModal, setShowModal] = useState(false)
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [idToDelete, setIdToDelete] = useState<number | null>(null)
+  const [newEvent, setNewEvent] = useState<Event>({
+    title: '',
+    start: '',
+    allDay: false,
+    id: 0
+  })
+
+  // Drag and Drop
+  useEffect(() => {
+    let draggableEl = document.getElementById('draggable-el')
+    if (draggableEl) {
+      new Draggable(draggableEl, {
+        itemSelector: ".fc-event",
+        eventData: function (eventEl) {
+          let title = eventEl.getAttribute("title")
+          let id = eventEl.getAttribute("data")
+          let start = eventEl.getAttribute("start")
+          return { title, id, start }
+        }
+      })
+    }
+  }, [])
+
   return (
     <>
     <nav className="flex justify-between mb-12 border-b border-violet-100 p-4">
@@ -27,7 +69,7 @@ export default function Home() {
                 center: 'title',
                 right: 'resourceTimelineWook, dayGridMonth,timeGridWeek'
               }}
-              events={{}}
+              events={allEvents}
               nowIndicator={true}
               editable={true}
               droppable={true}
@@ -38,6 +80,19 @@ export default function Home() {
               // eventClick={}
             />
           </div>
+            <div id="draggable-el" className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50">
+              <h1 className="font-bold text-lg text-center">Drag Event</h1>
+                {events.map(event => (
+                  <div
+                    className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
+                    title={event.title}
+                    key={event.id}
+                  >
+                    {event.title}
+                  </div>
+                  ))}
+            </div>
+        
        </div>
     </main>
     </>
